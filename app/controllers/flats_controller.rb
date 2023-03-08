7ac69fb48a6a0dd45fb9bc347476fe0d26052292
Shona Chan <mojavealoe@gmail.com>
2023-03-09 01:16:32 +0800

    gets coordinates from forward geocoding mapbox api

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,7 @@
+require 'open-uri'
+
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+  before_action :set_flat, only: %i[show edit update destroy coordinates_array]
 
   # GET /flats
   def index
@@ -8,7 +10,7 @@ class FlatsController < ApplicationController
 
   # GET /flat/:id
   def show
-    @flat
+    coordinates_array
   end
 
   # GET /flats/new
@@ -25,6 +27,7 @@ class FlatsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
 
   def edit
     @flat
@@ -52,4 +55,14 @@ class FlatsController < ApplicationController
   def set_flat
     @flat = Flat.find(params[:id])
   end
+
+  def coordinates_array
+    address = CGI.escape @flat.address
+    api_key = 'pk.eyJ1Ijoid2VlcG90dHkiLCJhIjoiY2wyMGZlZ3Y1MHZzOTNrbXFnejJ3bnEwZyJ9.Oa2SzwF6YKqbLEQJvaTXFA'
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{address}.json?access_token=#{api_key}"
+    data = JSON.parse(URI.open(url).read)
+    data['features'].first['center']
+  end
+
+
 end
