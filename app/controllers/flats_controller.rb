00b59f492462f7d1ff2eca38e2fa2fd94b7adf68
Shona Chan <mojavealoe@gmail.com>
2023-03-08 17:18:08 +0800

    displays flat details on show page:

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,13 @@
 class FlatsController < ApplicationController
+  # GET /flats
   def index
     @flats = Flat.all
   end
 
+  # GET /flat/:id
   def show
     @flat = Flat.find(params[:id])
   end
+
+
 end
